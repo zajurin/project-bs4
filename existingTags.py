3bd2727eb1de3759d25cbf01e3834c84512761ed
zajurin <43732868+zajurin@users.noreply.github.com>
2021-10-03 19:49:42 -0500

    minimal changes
    updating minimal changes

--- existingTags.py
+++ existingTags.py
@@ -11,7 +11,6 @@ import re
 import urllib.request
 import phonenumbers
 
-
 #******* AVOID BEING BLOCKED *******
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
 
@@ -25,7 +24,6 @@ myCurrentURL = 'https://google.com/search?q=' + text
 #ACCESING TO URL's INFO
 response = requests.get(myCurrentURL, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
-
 
 tagsSeparatedByComma = [tag.name for tag in soup.find_all()]
 tagsWithClasses= [str(tag) for tag in soup.find_all("h3")]
